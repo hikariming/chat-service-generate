ebc385de3d09fe46cee670de4b94f40d4870606f
root <313338264@qq.com>
2023-09-11 15:36:51 +0800

    优化东西

--- src/currproj.mjs
+++ src/currproj.mjs
@@ -1,10 +1,37 @@
 import { startOpenAI } from "./openaikey.mjs";
 import fs from "fs";
-import { execSync } from 'child_process';
 import inquirer from 'inquirer';
 import path from 'path';
 
 
+function GetCodeFromGPTResponse(text,FilePath) {
+
+// 正则表达式用于匹配\`\`\`后的任何字符，直到另一个\`\`\`出现
+const regex = /```[\s\S]*?```/g;
+const matchedCode = text.match(regex);
+
+if (matchedCode) {
+  // 将\`\`\`和语言标识符（如typescript）从代码字符串中移除
+  const cleanedCode = matchedCode[0].replace(/```[a-zA-Z]*\n/, "").replace(/```/, "");
+  // save code to moduleContent file
+   // 打开现有的File以获取其当前内容
+   const existingContent = fs.readFileSync(FilePath, 'utf8');
+  
+   // 添加GPT-3生成的代码到现有的moduleFile内容中
+   const newContent = existingContent + '\n' + cleanedCode;
+ 
+   // 保存新的moduleFile内容
+   fs.writeFileSync(FilePath, newContent, 'utf8');
+   console.log(cleanedCode);
+
+ 
+   console.log(`GPT-3 generated code has been saved to ${FilePath}`);
+   
+} else {
+  console.log("No code found");
+}
+
+}
 
 export async function generateMongoSchema() {
   console.log("Generating MongoDB schema...");
@@ -198,34 +225,79 @@ export async function generateCRUD(answers) {
 const text = gptResponse.choices[0].message.content.trim()
 console.log("GPT response:", text);
 
+GetCodeFromGPTResponse(text,moduleFile)
 
-// 正则表达式用于匹配\`\`\`后的任何字符，直到另一个\`\`\`出现
-const regex = /```[\s\S]*?```/g;
-const matchedCode = text.match(regex);
+// 开始生成service
+let ServiceContent = fs.readFileSync(serviceFile, 'utf8');
 
-if (matchedCode) {
-  // 将\`\`\`和语言标识符（如typescript）从代码字符串中移除
-  const cleanedCode = matchedCode[0].replace(/```[a-zA-Z]*\n/, "").replace(/```/, "");
-  // save code to moduleContent file
-   // 打开现有的moduleFile以获取其当前内容
-   const existingModuleContent = fs.readFileSync(moduleFile, 'utf8');
-  
-   // 添加GPT-3生成的代码到现有的moduleFile内容中
-   const newModuleContent = existingModuleContent + '\n' + cleanedCode;
- 
-   // 保存新的moduleFile内容
-   fs.writeFileSync(moduleFile, newModuleContent, 'utf8');
-   console.log(cleanedCode);
-
- 
-   console.log("GPT-3 generated code has been saved to moduleFile");
-   
-} else {
-  console.log("No code found");
-}
+ // 编写prompt
+ const promptService = `
+ 现在有一个nest文件${serviceFile} 代码是:
+ ${ServiceContent}
+ 现在有一个文件${selectedSchemaFilePath}，代码是:
+ ${schemaContent}
+ 我已经在moudle里面加入的对selectedSchemaFilePath的引入，帮我编写CRUD接口：
+ 1、在import中加入相关import
+ 2、在构造函数中加入相关的引入
+ 3、在service中修改相关的方法
+ 直接生成代码
+`;
 
 
+const gptResponseService = await openai.chat.completions.create({
+  messages: [
+    {
+      role: "system",
+      content:
+        "You are a helpful assistant that can generate code.",
+    },
+    {
+      role: "user",
+      content: promptService,
+    },
+  ],
+  model: "gpt-3.5-turbo", // 使用适当的模型
+});
+const textService = gptResponse.choices[0].message.content.trim()
+console.log("GPT response:", textService);
 
+GetCodeFromGPTResponse(textService,serviceFile)
+
+//开始生成controller
+
+let ControllerContent = fs.readFileSync(controllerFile, 'utf8');
+
+  // 编写prompt
+   // 编写prompt
+ const promptController = `
+ 现在有一个nest service文件${serviceFile} 代码是:
+ ${ServiceContent}
+ 现在有一个controller文件${controllerFile}，代码是:
+ ${ControllerContent}
+ 帮我编写${controllerFile}的CRUD Controller接口：
+ 1、在import中加入相关import
+ 3、在Controller中修改相关的方法
+ 直接生成代码
+`;
+
+const gptResponseController = await openai.chat.completions.create({
+  messages: [
+    {
+      role: "system",
+      content:
+        "You are a helpful assistant that can generate code.",
+    },
+    {
+      role: "user",
+      content: promptService,
+    },
+  ],
+  model: "gpt-3.5-turbo", // 使用适当的模型
+});
+const textController = gptResponse.choices[0].message.content.trim()
+console.log("GPT response:", textController);
+
+GetCodeFromGPTResponse(textController,controllerFile)
 
 
   
